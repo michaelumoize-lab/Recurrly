e74c219eaef6bba34d4f6f8f3fc5770bcc840393
Michael Umoize <michaelumoize@gmail.com>
2026-07-26 19:47:29 +0300

    feat: add custom fonts and update layout for improved typography

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,32 +9,24 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
       <Link
-        className="mt-4 rounded bg-primary text-white pt-4 pb-4 pl-8 pr-8"
+        className="mt-4 rounded bg-primary font-sans-bold text-white pt-4 pb-4 pl-8 pr-8"
         href="/onboarding"
       >
         Go to onboarding
       </Link>
       <Link
-        className="mt-4 rounded bg-primary text-white pt-4 pb-4 pl-8 pr-8"
+        className="mt-4 rounded bg-primary font-sans-bold text-white pt-4 pb-4 pl-8 pr-8"
         href="/(auth)/sign-in"
       >
         Go to SignIn
       </Link>
       <Link
-        className="mt-4 rounded bg-primary text-white pt-4 pb-4 pl-8 pr-8"
+        className="mt-4 rounded bg-primary font-sans-bold text-white pt-4 pb-4 pl-8 pr-8"
         href="/(auth)/sign-up"
       >
         Go to SignUp
-      </Link>
-      <Link href="/subscriptions/spotify">Spotify Subscriptions</Link>
-      <Link
-        href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}
-      >
-        Claude Max Subscriptions
       </Link>
     </SafeAreaView>
   );
